8c2f38b23f96bf1e1cccef2cf0e6e309ffda1936
Greg Magdsick <greg.magdsick@gmail.com>
2016-01-18 15:34:13 -0800

    displays one random picture

--- js/app.js
+++ js/app.js
@@ -25,16 +25,21 @@ var bag = new Product('Bag','img/bag.jpg');
 var banana = new Product('Banana', 'img/banana.jpg');
 var boots = new Product('Boots', 'img/banana.jpg');
 var chair = new Product('Chair', 'img/chair.jpg');
-var cthulu = new Product('Cthulu', 'img/cthulu.jpg');
+var cthulhu = new Product('Cthulu', 'img/cthulhu.jpg');
 var dragon = new Product('Dragon Meat', 'img/dragon.jpg');
 var pen = new Product('Pen Silverware', 'img/pen.jpg');
 var scissors = new Product('Pizza Scissors', 'img/scissors.jpg');
 var shark = new Product('Shark Sleepig Bag', 'img/shark.jpg');
 var sweep = new Product('Sweep', 'img/sweep.png');
 var unicorn = new Product('Unicorn Meat', 'img/unicorn.jpg');
-var usb = new Product('USB Tentacle', 'img/usb.jpg');
+var usb = new Product('USB Tentacle', 'img/usb.gif');
 var water_can = new Product('Watering Can', 'img/water-can.jpg');
 var wine_glass = new Product('Wine Glass', 'img/wine-glass.jpg');
 
+function randomImageDisplay() {
+  var i = Math.ceil(Math.random() * (allProducts.length - 1));
+  allProducts[i].displayImage();
+}
 
-bag.displayImage();
+
+randomImageDisplay();
